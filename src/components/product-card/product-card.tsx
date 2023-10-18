6516bd6fb90db2640ee34511097661c68948093e
flydecisive <flydecisive@yandex.ru>
2023-10-18 22:29:56 +0300

    добавляет шрифты

---
+++ src/components/product-card/product-card.tsx
@@ -0,0 +1,26 @@
+import styles from "./product-card.module.css";
+
+interface ProductCardProps {
+  header: string;
+  price: string;
+  city: string;
+  time: string;
+}
+
+function ProductCard({ header, price, city, time }: ProductCardProps) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.card_img}></div>
+      <div className={styles.wrapper}>
+        <h2 className={styles.header}>{header}</h2>
+        <p className={styles.price}>{price} ₽</p>
+        <div className={styles.data_items}>
+          <p className={styles.data_item}>{city}</p>
+          <p className={styles.data_item}>{time}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,15 +1,32 @@
 import styles from "./main.module.css";
 import Header from "../../components/header/header";
 import PageNav from "../../components/page-nav/page-nav";
+import ProductCard from "../../components/product-card/product-card";
+import { cardData } from "../../data";
 
 function Main() {
   const buttonsNames = ["Вход в личный кабинет"];
+  const cardsItems = cardData.map((el, index) => {
+    return (
+      <ProductCard
+        key={index}
+        header={el.header}
+        price={el.price}
+        city={el.city}
+        time={el.time}
+      />
+    );
+  });
 
   return (
     <div className={`${styles.main}`}>
       <Header names={buttonsNames} />
       <div className={`${styles.wrapper} center`}>
         <PageNav isSearch={true} />
+        <div className={styles.content}>
+          <h1 className={styles.title}>Объявления</h1>
+          <div className={styles.cards}>{cardsItems}</div>
+        </div>
       </div>
     </div>
   );
