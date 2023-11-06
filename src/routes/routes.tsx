236ac08e6a6078fdd26a95d26fb7f0db5cccbbd3
Maksim <45427901+flydecisive@users.noreply.github.com>
2023-11-06 17:10:42 +0300

    добавляет страницу not-found

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,7 @@ import AuthorizationPage from "../pages/authorization/authorization";
 import ProfilePage from "../pages/profile/profile";
 import AdvPage from "../pages/adv/adv";
 import SellerProfilePage from "../pages/seller-profile/seller-profile";
+import NotFound from "../pages/not-found/not-found";
 
 interface AppRoutesProps {
   isAllowed: boolean;
@@ -22,6 +23,7 @@ function AppRoutes({ isAllowed }: AppRoutesProps) {
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<AuthorizationPage />} />
       <Route path="/register" element={<AuthorizationPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
